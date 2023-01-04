84e6cae2073f5d0aedc5691b02641a82a6f14b53
Andriy <andriy.andriyan1@gmail.com>
2023-01-04 14:27:56 +0700

    Update logo and favicon

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,9 @@
+import Image from 'next/image';
 import { FC, memo } from 'react';
 
 const Header: FC = () => (
-  <header className="w-full flex p-4 border-b fixed top-0 left-0 z-10 bg-white">
-    <div>Beranda Muslim</div>
+  <header className="w-full flex px-4 py-2 border-b fixed top-0 left-0 z-10 bg-white">
+    <Image src="/berandamuslim-logo.png" alt="Logo Beranda Muslim" height={50} width={134} />
   </header>
 );
 
